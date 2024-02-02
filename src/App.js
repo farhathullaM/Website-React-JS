7df5ab28df796fa91deae70a5611e2fba3eee92d
farhathulla <farhathulla01@gmail.com>
2024-02-02 12:27:51 +0530

    nineth commit

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
           <Route path=':productId' element={<Product/>}/>
         </Route>
       <Route path='/cart' element={<Cart/>}/>
-      <Route path='/cart' element={<LoginSignup/>}/>
+      <Route path='/login' element={<LoginSignup/>}/>
       </Routes>
       <Footer/>
       </BrowserRouter>
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import './CSS/LoginSignup.css';
 
 const LoginSignup = () => {
   return (
-    <div>
-      
+    <div className='loginsingup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-input">
+          <input type="text" placeholder='username'/>
+          <input type="email" placeholder='email' />
+          <input type="password" placeholder='password'/>
+        </div>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>I agree to follow the terms and conditions provided.</p>
+        </div>
+        <button>Continue</button>
+        <div className='loginsignup-login'>
+        <p >Already have an account <span>Login</span></p>
+        </div>
+
+      </div>
     </div>
   )
 }
