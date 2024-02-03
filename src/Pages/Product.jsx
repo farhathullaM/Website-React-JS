b5f2aa45ae2e5d74482f9750865d4e56c3a95306
farhathulla <farhathulla01@gmail.com>
2024-02-03 08:44:04 +0530

    tenth commit

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ShopContext} from '../Context/ShopContext';
+import all_product from '../Components/Assets/all_product';
+import Breadcrum from '../Components/Breadcrums/Breadcrum';
+import { useParams} from 'react-router-dom';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
+import DisciptionBox from '../Components/DiscriptionBox/DisciptionBox';
 
 const Product = () => {
+
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId));
   return (
     <div>
-      
+      <Breadcrum product={product} />
+      <ProductDisplay product={product} />
+      <DisciptionBox />
     </div>
   )
 }
